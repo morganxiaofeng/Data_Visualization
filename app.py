accd9f22fb789b5a85a8e69f8051e9e80ec0ea67
morganxiaofeng <93844949+morganxiaofeng@users.noreply.github.com>
2022-03-26 18:25:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,48 @@ import altair as alt
 # from altair import datum
 import datetime
 
+data = pd.read_csv('owid-covid-data_final.csv')
+
+def display_sidebar(data):
+    sel_region,sel_index = None, None
+
+    st.sidebar.header('Choose options below')
+    # 0) Need to reset data
+    st.sidebar.markdown('Reset dataset?')
+    if st.sidebar.button(label='Clear cache'):
+        st.caching.clear_cache()
+        st.experimental_rerun()
+
+    # 1) Choose a Region/Country to display
+    st.sidebar.markdown('Choose a Continent (e.g., Europe)')
+    
+    sel_region = st.sidebar.selectbox('Continent',['World', 'Africa', 'Asia', 'Europe', 'North America', 'Oceania', 'South America'])
+
+    # 2) Choose a Political Index
+    st.sidebar.markdown('Choose a Political Index (e.g., Stringency Index)')
+    
+    sel_index = st.sidebar.selectbox('Political Index', ['Stringency Index', 'GDP per Capita', 'Human Development Index'])
+        
+    # 3) Choose a date to display
+    st.sidebar.header('Choose a date below')
+    st.sidebar.markdown('Choose a date (e.g., 2020-08-15)')
+    sel_date = st.sidebar.date_input('Date:', datetime.date(2021,12,31))
+    st.write(sel_date)
+     
+    return sel_region, sel_index, sel_date
+
+
+
+
+
+index_dict = {'Stringency Index': 'stringency_index', 'GDP per Capita': 'gdp_per_capita', 'Human Development Index': 'human_development_index'}
 def color_scale(val):
     for i, b in enumerate(breaks):
         if val <= b:
             return color_range[i]
     return color_range[i]
 
-stringency = pd.read_csv('owid-covid-data_final.csv').loc[:,['iso_code','location','date','stringency_index']].rename(columns={'iso_code':'adm0_a3'})
+index = pd.read_csv('owid-covid-data_final.csv').loc[:,['iso_code', 'date', 'stringency_index', 'gdp_per_capita', 'human_development_index']].rename(columns={'iso_code':'adm0_a3'})
 
 src_geo = 'countries.json'
 json_geo = pd.read_json(src_geo)
@@ -27,7 +62,7 @@ df["coordinates"] = json_geo["features"].apply(lambda row: row["geometry"]["coor
 df["name"] = json_geo["features"].apply(lambda row: row["properties"]["name"])
 df["adm0_a3"] = json_geo["features"].apply(lambda row: row["properties"]["adm0_a3"])
 df["admin"] = json_geo["features"].apply(lambda row: row["properties"]["admin"])
-df = pd.merge(stringency, df, on='adm0_a3', how='left')
+df = pd.merge(index, df, on='adm0_a3', how='left')
 df['date'] = pd.to_datetime(df['date'])
 
 breaks = [.0, .2, .4, .6, .8, 1]
@@ -40,22 +75,16 @@ color_range = [
     [49,130,189],
     [8,81,156],
 
-        # # 6-class Purples (For reference)
-        # [242,240,247],
-        # [218,218,235],
-        # [188,189,220],
-        # [158,154,200],
-        # [117,107,177],
-        # [84,39,143],
+    # # 6-class Purples (For reference)
+    # [242,240,247],
+    # [218,218,235],
+    # [188,189,220],
+    # [158,154,200],
+    # [117,107,177],
+    # [84,39,143],
     ]
 
-df['fill_color'] = (df['stringency_index']/df['stringency_index'].max()).replace(np.nan,0).apply(color_scale)
-
-# Choose a startdate to display
-st.sidebar.header('Choose a startdate below')
-st.sidebar.markdown('Choose a startdate (e.g., 2020-08-15)')
-sel_date = st.sidebar.date_input('Date:', datetime.date(2021,12,31))
-st.write(sel_date)
+df['fill_color'] = (df[index_dict[sel_index]]/df[index_dict[sel_index]].max()).replace(np.nan,0).apply(color_scale)
 
 if sel_date <= datetime.date(2021,12,31) and sel_date >= datetime.date(2021,1,1):
     st.success(f'Date: {sel_date}')
@@ -88,7 +117,7 @@ polygon_layer = pdk.Layer(
 view_state = pdk.ViewState(latitude=0, longitude=0, zoom=1, bearing=0, pitch=0)
 
 # Render
-tooltip = {"html": "<b>Country/Region:</b> {admin} <br /><b>{Stringency Index}:</b> {stringency_index}"}
+tooltip = {"html": "<b>Country/Region:</b> {admin} <br /> {sel_index}: {index_dict[sel_index]}"}
 r = pdk.Deck(layers=[polygon_layer], initial_view_state=view_state, map_style='light', tooltip=tooltip)
 
 st.pydeck_chart(r, use_container_width=True)
