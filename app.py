957eb6ad911e4b3ba938768805446130dc4ac12b
morganxiaofeng <93844949+morganxiaofeng@users.noreply.github.com>
2022-03-28 02:15:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ vac_heatmap = alt.Chart(df_vac_case).mark_rect().encode(
                                 color=colorvac,
                                 opacity=opacity,
                                 tooltip=['Continent', 'Date', 'New Vaccinations Smoothed', 'New Cases Smoothed']
-                                ).add_selection(hover).interactive()
+                                ).add_selection(hover).interactive().configure_scale(bandPaddingInner=.1)
 
 st.altair_chart(vac_heatmap, use_container_width=True)
 
@@ -187,7 +187,7 @@ case_heatmap = alt.Chart(df_vac_case).mark_rect().encode(
                                 color=colorcas,
                                 opacity=opacity,
                                 tooltip=['Continent', 'Date', 'New Vaccinations Smoothed', 'New Cases Smoothed']
-                                ).add_selection(hover).interactive()
+                                ).add_selection(hover).interactive().configure_scale(bandPaddingInner=.1)
 
 st.altair_chart(case_heatmap, use_container_width=True)
 
